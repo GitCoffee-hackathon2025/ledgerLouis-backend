748744ae294511e70e4d06fdc45975eefa472f61
Chormiak <william.chormi@gmail.com>
2026-03-10 21:29:41 -0300

    FEAT: Adicionando swagger ao projeto
    Instalando e configurando o swagger com informações basicas, especialmente na descrição, sendo necessário atualiza-lo futuramente

--- src/modules/auth/schema.ts
+++ src/modules/auth/schema.ts
@@ -1,3 +1,22 @@
 // Uso do TypeBox
 
 // usado para validação das rotas e criação do Swagger. 
+
+/* O que é necessário para que o Swagger funcione de forma completa e é necessário que esse schema esteja dentro da rota
+schema: {
+  tags: ["auth"],
+  summary: "User login",
+
+  body: LoginBody,
+
+  params: ParamsSchema,
+  querystring: QuerySchema,
+  headers: HeaderSchema,
+
+  response: {
+    200: SuccessResponse,
+    401: UnauthorizedResponse,
+    400: ValidationError
+  }
+}
+*/
