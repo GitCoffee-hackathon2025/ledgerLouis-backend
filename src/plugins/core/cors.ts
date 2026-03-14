b1a9e5e92d131a91145cdad3248579c1f85edb70
Chormiak <william.chormi@gmail.com>
2026-03-14 15:51:08 -0300

    REFACTOR: Ativado as credenciais no cors

--- src/plugins/core/cors.ts
+++ src/plugins/core/cors.ts
@@ -2,18 +2,17 @@ import fp from "fastify-plugin";
 import fastifyCors from "@fastify/cors";
 import type { FastifyInstance } from "fastify";
 
-// Função que integra as variáveis de ambiente no fastify
-
 export default fp(
   async function (app: FastifyInstance) {
+    // Armazendo as origens no formato necessário para validação
     const allowedOrigins = new Set(
       app.config.ALLOWED_ORIGINS.split(",").map((o: string) => o.trim()),
     );
 
     await app.register(fastifyCors, {
       methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
-      // credentials: true // precisa relizar outras configurações de outros plugins antes
-      maxAge: 600,
+      credentials: true,
+      maxAge: 300, // Meia hora
       origin: (origin, cb) => {
         if (!origin) return cb(null, true);
 
