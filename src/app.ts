d1f764f64e499c60137e4a564f5f49f93cff081f
Chormiak <william.chormi@gmail.com>
2026-03-25 14:09:14 -0300

    FEAT: Adicionando o novo ajv e o plugin do banco no fastify (#4)

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,14 @@ import Autoload from "@fastify/autoload";
 
 import path from "path";
 
+// Função que cria a instância que permite maior validação com o Ajv
+import { createValidator } from "./lib/validator";
+
 // Core Plugins, só podem ser importados nesse arquivo e precisam ser carregados primeiro
 // Para maior organização importe-os na ordem correta de registro
 import env from "./plugins/core/env";
 import cors from "./plugins/core/cors";
+import db from "./plugins/core/db";
 
 /* 
 Para serviços pesados como emails e criação de pdf's será necessário instalar o BullMQ junto com o Redis e configura-los.
@@ -25,11 +29,18 @@ E para validação de headers das requisições: @fastify/helmet
 const root = __dirname;
 
 async function buildApp() {
+  const ajv = createValidator();
+
   const app = fastify({ logger: true });
 
+  app.setValidatorCompiler(({ schema }) => {
+    return ajv.compile(schema);
+  });
+
   // Plugins fundamentais para o carregamento
-  await app.register(env);
+  await app.register(env, { ajv });
   await app.register(cors);
+  await app.register(db);
 
   // Plugins mais isolados
   await app.register(Autoload, {
