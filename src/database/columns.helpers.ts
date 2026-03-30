b3619a7c1b9b2a2f045e8c0927ef7e015ddbe98d
Chormiak <william.chormi@gmail.com>
2026-03-30 15:03:50 -0300

    REFACTOR: Declarando o campo id como função para uso (#6)

--- src/database/columns.helpers.ts
+++ src/database/columns.helpers.ts
@@ -2,10 +2,10 @@ import { char, timestamp } from "drizzle-orm/mysql-core";
 
 import { type ULID, generateId } from "../lib/id";
 
-export const id = char("id", { length: 26 })
-  .$type<ULID>()
-  .primaryKey()
-  .$defaultFn(() => generateId());
+export const ulid = (name: string) =>
+  char(name, { length: 26 })
+    .$type<ULID>()
+    .$defaultFn(() => generateId());
 
 export const timestamps = {
   createdAt: timestamp().defaultNow().notNull(),
