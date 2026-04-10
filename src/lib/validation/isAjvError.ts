e3c80b2fcd678d75614bc566f837d80128bbca13
Chormiak <william.chormi@gmail.com>
2026-04-10 08:46:09 -0300

    FEAT: Criando verificação de erro pertencente ao Ajv

---
+++ src/lib/validation/isAjvError.ts
@@ -0,0 +1,12 @@
+import { ErrorObject } from "ajv";
+
+export function isAjvError(
+  error: unknown,
+): error is { validation: ErrorObject[] } {
+  return (
+    typeof error === "object" &&
+    error !== null &&
+    "validation" in error &&
+    Array.isArray((error as any).validation)
+  );
+}
