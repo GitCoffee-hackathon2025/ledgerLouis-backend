a983e6bcce9e865dc7ab18d42269c4438ddff84e
Chormiak <william.chormi@gmail.com>
2026-06-19 13:34:59 -0300

    PERF: Retirando reduncionais do dns ipv4 (#32)

--- src/types/db.ts
+++ src/types/db.ts
@@ -1,6 +1,4 @@
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 import * as schema from "../database/schemas/index.js";
-import dns from "dns";
 
-dns.setDefaultResultOrder("ipv4first");
 export type DB = NodePgDatabase<typeof schema>;
