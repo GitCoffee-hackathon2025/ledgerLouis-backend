87df6b6e1bfa28aeeb0c8c127fd5ad4801469b70
Chormiak <william.chormi@gmail.com>
2026-07-15 14:13:21 -0300

    BUILD: Jogando configuração do dotenv para o server.ts (#39)

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 import dns from "dns";
 dns.setDefaultResultOrder("ipv4first");
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import buildApp from "./src/app.js";
 
 async function start() {
@@ -9,7 +12,7 @@ async function start() {
   await app.ready();
 
   try {
-    await app.listen({ port: app.config.PORT, host: '0.0.0.0' });
+    await app.listen({ port: app.config.PORT, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
