7de967bb8323ca8edb90299b281c96577eb6e3f6
Chormiak <william.chormi@gmail.com>
2026-07-24 11:57:14 -0300

    REFACTOR: Criando rascunho de ratoração com a nova infraestrutura de storage (#47)

--- src/api/plugins/core/storage.ts
+++ src/api/plugins/core/storage.ts
@@ -1,11 +1,14 @@
-// src/plugins/core/storage.ts
 import fp from "fastify-plugin";
+import type { FastifyInstance } from "fastify";
 
 import { CloudinaryStorageProvider } from "../../../modules/uploader/cloudinaryStorage.js";
 import { LocalStorageProvider } from "../../../modules/uploader/localStorage.js";
 
+import { createStorageService } from "../../../infrastructure/storage/index.js";
+import { fromRoot } from "../../../config/paths.js";
+
 export default fp(
-  async function (app) {
+  async function (app: FastifyInstance) {
     // const storage =
     //   app.config.STORAGE_DRIVER === "cloudinary"
     //     ? new CloudinaryStorageProvider(app.cloudinary)
@@ -25,3 +28,29 @@ export default fp(
     dependencies: ["env", "cloudinary"],
   },
 );
+
+async function teste(app: FastifyInstance) {
+  let storage: ReturnType<typeof createStorageService>;
+  const driver = app.config.STORAGE_DRIVER;
+
+  switch (driver) {
+    case "local":
+      storage = createStorageService(driver, {
+        root: fromRoot("uploads"),
+      });
+
+      break;
+    case "cloudinary":
+      storage = createStorageService(driver, {
+        cloud_name: app.config.CLOUDINARY_CLOUD_NAME,
+        api_key: app.config.CLOUDINARY_API_KEY,
+        api_secret: app.config.CLOUDINARY_API_SECRET,
+      });
+
+      break;
+    default:
+      throw new Error(`Unknown storage provider "${driver}".`);
+  }
+
+  // app.decorate("storage", storage);
+}
