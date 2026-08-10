d99dfe7647374cf77353b9c284a7bd4f006a4545
Chormiak <william.chormi@gmail.com>
2026-08-10 14:48:13 -0300

    FEAT: Criando plugin de provedor do restapi (#47)"

--- src/api/plugins/core/storage.ts
+++ src/api/plugins/core/storage.ts
@@ -1,56 +1,44 @@
 import fp from "fastify-plugin";
 import type { FastifyInstance } from "fastify";
 
-import { CloudinaryStorageProvider } from "../../../modules/uploader/cloudinaryStorage.js";
-import { LocalStorageProvider } from "../../../modules/uploader/localStorage.js";
-
 import { createStorageService } from "../../../infrastructure/storage/index.js";
 import { fromRoot } from "../../../config/paths.js";
 
 export default fp(
   async function (app: FastifyInstance) {
-    // const storage =
-    //   app.config.STORAGE_DRIVER === "cloudinary"
-    //     ? new CloudinaryStorageProvider(app.cloudinary)
-    //     : new LocalStorageProvider();
+    let storage: ReturnType<typeof createStorageService> | undefined;
+    const driver = app.config.STORAGE_DRIVER;
 
-    // app.decorate("storage", storage);
+    switch (driver) {
+      case "local":
+        storage = createStorageService(driver, {
+          root: fromRoot("uploads"),
+        });
 
-    if (app.config.STORAGE_DRIVER === "cloudinary") {
-      app.decorate("storage", new CloudinaryStorageProvider(app.cloudinary));
-      return;
+        break;
+      case "cloudinary":
+        if (
+          !app.config.CLOUDINARY_CLOUD_NAME ||
+          !app.config.CLOUDINARY_API_KEY ||
+          !app.config.CLOUDINARY_API_SECRET
+        )
+          throw new Error("Cloudinary configuration is missing");
+
+        storage = createStorageService(driver, {
+          cloud_name: app.config.CLOUDINARY_CLOUD_NAME,
+          api_key: app.config.CLOUDINARY_API_KEY,
+          api_secret: app.config.CLOUDINARY_API_SECRET,
+        });
+
+        break;
+      default:
+        throw new Error(`Unknown storage provider "${driver}".`);
     }
 
-    app.decorate("storage", new LocalStorageProvider());
+    app.decorate("storage", storage);
   },
   {
     name: "storage",
-    dependencies: ["env", "cloudinary"],
+    dependencies: ["env", "multipart"],
   },
 );
-
-async function teste(app: FastifyInstance) {
-  let storage: ReturnType<typeof createStorageService>;
-  const driver = app.config.STORAGE_DRIVER;
-
-  switch (driver) {
-    case "local":
-      storage = createStorageService(driver, {
-        root: fromRoot("uploads"),
-      });
-
-      break;
-    case "cloudinary":
-      storage = createStorageService(driver, {
-        cloud_name: app.config.CLOUDINARY_CLOUD_NAME,
-        api_key: app.config.CLOUDINARY_API_KEY,
-        api_secret: app.config.CLOUDINARY_API_SECRET,
-      });
-
-      break;
-    default:
-      throw new Error(`Unknown storage provider "${driver}".`);
-  }
-
-  // app.decorate("storage", storage);
-}
