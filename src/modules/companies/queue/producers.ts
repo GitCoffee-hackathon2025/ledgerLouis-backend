b9f700c1dafab6b990b45bc8da0f8865f01a0d64
Chormiak <william.chormi@gmail.com>
2026-08-14 09:49:03 -0300

    REVERT: Retirando exemplo de estrutura para QUEUES (#47)

--- src/modules/companies/queue/producers.ts
+++
@@ -1,8 +0,0 @@
-import type { IRedisClient } from "bullmq";
-import { buildInviteProducer } from "./invite/index.js";
-
-export function buildCompanyProducers(connection: IRedisClient) {
-  return {
-    invite: buildInviteProducer(connection),
-  };
-}
