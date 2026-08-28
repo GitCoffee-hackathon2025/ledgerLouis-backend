09374e1f023f3fd80fd395292a5d0e1037d1fca7
Erick-Henrique-Pedrini <erickpedrinebr@gmail.com>
2026-08-28 01:18:54 -0300

    FEAT: test CORS

--- src/api/plugins/core/cors.ts
+++ src/api/plugins/core/cors.ts
@@ -4,28 +4,25 @@ import type { FastifyInstance } from "fastify";
 
 export default fp(
   async function (app: FastifyInstance) {
-    // Armazendo as origens no formato necessário para validação
-    const allowedOrigins = new Set(
-      app.config.ALLOWED_ORIGINS.split(",").map((o: string) => o.trim()),
-    );
+    const allowedOrigins = app.config.ALLOWED_ORIGINS
+      ?.split(",")
+      .map((origin) => origin.trim())
+      .filter(Boolean);
 
     await app.register(fastifyCors, {
       methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
       credentials: true,
-      maxAge: 300, // Meia hora
-      origin: (origin, cb) => {
-        if (!origin) return cb(null, true);
+      maxAge: 300,
 
-        if (
-          app.config.NODE_ENV === "development" &&
-          /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin)
-        )
-          return cb(null, true);
+      origin: allowedOrigins?.length
+        ? (origin, cb) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+              return cb(null, true);
+            }
 
-        if (allowedOrigins.has(origin)) return cb(null, true);
-
-        cb(new Error("Not allowed"), false);
-      },
+            cb(new Error("Not allowed"), false);
+          }
+        : true,
     });
   },
   {
