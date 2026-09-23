6d97591e227b6f129c1798555ae5f0e69c699331
Chormiak <william.chormi@gmail.com>
2026-09-23 14:38:50 -0300

    FEAT: Registrando o worker de verificação de email (#47)

--- src/worker/loader.ts
+++ src/worker/loader.ts
@@ -1,10 +1,12 @@
+import { authWorkers } from "../modules/auth/queue/workers.js";
 import { fileWorkers } from "../modules/files/queue/workers.js";
+import { companyWorkers } from "../modules/companies/queues/workers.js";
 
 import { fileSchedulers } from "../modules/files/queue/schedulers.js";
-import { companyWorkers } from "../modules/companies/queues/workers.js";
 
 // Workers
 export const builds = {
+  auth: authWorkers,
   files: fileWorkers,
   companies: companyWorkers,
 };
